a860eaffd59e9b3d1b86e0a64c9612bc680722ee
Dragons0458 <juacase5826@gmail.com>
2024-04-06 20:52:07 -0500

    Haz generado la función que necesito, ahora, podrías generar el HTML (Basado en el seed), el CSS y el JS necesario para poder usarla?
    Para ello me gustaría que uses como base la imagen que te estoy adjuntando, la cual contiene un diseño de cómo debería verse y funcionar la página.

--- reverse-string/script.js
+++ reverse-string/script.js
@@ -1,3 +1,20 @@
+const inputString = document.getElementById('inputString');
+const reverseButton = document.getElementById('reverseButton');
+const outputString = document.getElementById('outputString');
+const copyButton = document.getElementById('copyButton');
+
+reverseButton.addEventListener('click', () => {
+	const originalString = inputString.value;
+	const reversedString = reverseString(originalString);
+	outputString.textContent = reversedString;
+});
+
+copyButton.addEventListener('click', () => {
+	const reversedString = outputString.textContent;
+	navigator.clipboard.writeText(reversedString);
+	alert('Reversed string copied to clipboard!');
+});
+
 function reverseString(str) {
 	return str.split('').reverse().join('');
 }
